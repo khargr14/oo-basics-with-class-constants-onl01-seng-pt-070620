6bbad9caeb6f9e710a0acbd51867444b470a86fa
Keesha Hargrove <kharg14@gmail.com>
2020-07-23 22:53:40 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -11,3 +11,20 @@ class Book
   end
 
 end
+
+
+
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def cobble
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+
+end
